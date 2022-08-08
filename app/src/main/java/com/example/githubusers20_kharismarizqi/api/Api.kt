ff6d5561933bdf39ac632b7f3c1b8039f242069e
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-08-08 00:44:41 -0300

    Salvar Projeto

--- app/src/main/java/com/example/githubusers20_kharismarizqi/api/Api.kt
+++ app/src/main/java/com/example/githubusers20_kharismarizqi/api/Api.kt
@@ -4,11 +4,12 @@ import com.example.githubusers20_kharismarizqi.ui.data.model.UserResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.HEAD
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @HEAD("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUsers(
         @Query("q") query: String
     ): Call<UserResponse>
--- app/src/main/java/com/example/githubusers20_kharismarizqi/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/githubusers20_kharismarizqi/ui/main/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.githubusers20_kharismarizqi.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
-import android.widget.Adapter
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubusers20_kharismarizqi.R
@@ -18,8 +16,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     /*Global declare variables: */
-    private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: UserAdapter
+    private var viewModel: MainViewModel? = null
+    private var adapter: UserAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,24 +28,53 @@ class MainActivity : AppCompatActivity() {
         /* Functions: */
         hideActionBar()
         centerActionBar()
-//        imageViewSearchUser()
-//        editTexWriteUser()
+        imageViewSearchUser()
+        editTexWriteUser()
+        getSearchUser()
+        recyclerViewSettings()
+        settings()
+    }
 
-        /* Variables: */
+    private fun settings() {
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(MainViewModel::class.java)
+    }
+
+    private fun recyclerViewSettings() {
         adapter = UserAdapter()
-        adapter.notifyDataSetChanged()
-        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
-            .get(MainViewModel::class.java)
-
+        adapter!!.notifyDataSetChanged()
         rcclrVw_actvtMain_id.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rcclrVw_actvtMain_id.setHasFixedSize(true)
         rcclrVw_actvtMain_id.adapter = adapter
+    }
 
+    private fun imageViewSearchUser() {
         imgVw_search_actvtMain_id.setOnClickListener {
             searchUser()
         }
+    }
 
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            prgrssBar_actvtMain_id.visibility = View.VISIBLE
+        } else {
+            prgrssBar_actvtMain_id.visibility = View.GONE
+        }
+    }
+
+    private fun getSearchUser() {
+        viewModel?.getSearchUsers()?.observe(this) {
+            if (it != null) {
+                adapter?.setList(it)
+                showLoading(false)
+            }
+        }
+    }
+
+    private fun editTexWriteUser() {
         edtTxt_searchUser_actvtMain_id.setOnKeyListener { v, keycode, event ->
             if (event.action == KeyEvent.ACTION_DOWN && keycode == KeyEvent.KEYCODE_ENTER) {
                 searchUser()
@@ -55,54 +82,13 @@ class MainActivity : AppCompatActivity() {
             }
             return@setOnKeyListener false
         }
-
-        viewModel.getSearchUsers().observe(this
-        ){
-            if (it != null) {
-                adapter.setList(it)
-                showLoading(false)
-            }
-        }
-
-//        viewModel.getSearchUsers().observe(this) {
-//
-//            if (it != null) {
-//                adapter.setList(it)
-//                showLoading(false)
-//            }
-//        }
-
     }
-
-//    private fun imageViewSearchUser() {
-//        imgVw_search_actvtMain_id.setOnClickListener {
-//            searchUser()
-//        }
-//    }
-
-//    private fun editTexWriteUser() {
-//        edtTxt_searchUser_actvtMain_id.setOnKeyListener { v, keycode, event ->
-//            if (event.action == KeyEvent.ACTION_DOWN && keycode == KeyEvent.KEYCODE_ENTER) {
-//                searchUser()
-//                return@setOnKeyListener true
-//            }
-//            return@setOnKeyListener false
-//        }
-
 
     private fun searchUser() {
         val query = edtTxt_searchUser_actvtMain_id.text.toString()
         if (query.isEmpty()) return
         showLoading(true)
-        viewModel.setSearchUsers(query)
-    }
-
-    private fun showLoading(state: Boolean) {
-        if (state) {
-            prgrssBar_actvtMain_id.visibility = View.VISIBLE
-        } else {
-            prgrssBar_actvtMain_id.visibility = View.INVISIBLE
-        }
+        viewModel?.setSearchUsers(query)
     }
 
     private fun centerActionBar() {
