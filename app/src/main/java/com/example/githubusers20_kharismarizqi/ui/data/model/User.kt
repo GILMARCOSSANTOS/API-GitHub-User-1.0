152746ae8e021525d5a83a7368776ce5bc7d35c0
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-08-08 01:19:27 -0300

    Salvar Branch Celso Furtado

--- app/src/main/java/com/example/githubusers20_kharismarizqi/ui/data/model/User.kt
+++ app/src/main/java/com/example/githubusers20_kharismarizqi/ui/data/model/User.kt
@@ -1,9 +1,14 @@
 package com.example.githubusers20_kharismarizqi.ui.data.model
 
 data class User(
+
+    /* 01 = CF: Atributos da classe dentista */
     val login: String,
     val id: Int,
     val avatar: String
 ) {
-
+    /* 01 = CF: Método 'To String = Code + Generate + ToString': */
+    override fun toString(): String {
+        return "User(login='$login', id=$id, avatar='$avatar')"
+    }
 }
